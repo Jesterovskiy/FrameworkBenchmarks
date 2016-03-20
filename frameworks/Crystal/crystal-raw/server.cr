838a1fb8546ac03371dc8706d373c4850cf935e3
Jesterovskiy <jester@aejis.eu>
2016-03-20 09:41:06 +0200

    Fix Crystal raw

--- frameworks/Crystal/crystal-raw/server.cr
+++ frameworks/Crystal/crystal-raw/server.cr
@@ -1,17 +1,17 @@
 require "http/server"
 require "json"
 
-server = HTTP::Server.new(8080) do |request|
+server = HTTP::Server.new(8080) do |context|
   headers = HTTP::Headers{"Server": "Crystal", "Date": Time.utc_now.to_s}
-  case request.path
+  case context.request.path
   when "/json"
     headers.add("Content-Type", "application/json")
-    HTTP::Response.new 200, {message: "Hello, World!"}.to_json, headers
+    HTTP::Client::Response.new 200, {message: "Hello, World!"}.to_json, headers
   when "/plaintext"
     headers.add("Content-Type", "text/plain")
-    HTTP::Response.new 200, "Hello, world!", headers
+    HTTP::Client::Response.new 200, "Hello, world!", headers
   else
-    HTTP::Response.not_found
+    HTTP::Client::Response.not_found
   end
 end
 
